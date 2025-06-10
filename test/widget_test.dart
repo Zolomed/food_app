ee388cfd5846662c8d7a79c8db7346fe02d419bb
Zolomed <sasha.zolotarev10@gmail.com>
2025-06-10 22:49:49 +0300

    feat(тесты): добавлен тест для отображения названия и цены в FoodCard

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,32 +1,32 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-//TODO написать тесты
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:food_app/main.dart';
+import 'package:food_app/widgets/food_card.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  // Тест: Проверка FoodCard — отображение названия и цены блюда
+  testWidgets('FoodCard отображает название и цену',
+      (WidgetTester tester) async {
+    // Создаём тестовый FoodCard
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: FoodCard(
+            image:
+                'https://www.sortiraparis.com/hotel-restaurant/restaurant/articles/281096-sphere-le-beau-restaurant-gastronomique-paris-8e-se-devoile', // Подставьте любой валидный URL
+            name: 'Тестовое блюдо',
+            price: 123.45,
+            isFavorite: false,
+            quantity: 0,
+            onFavoriteTap: () {},
+            onAdd: () {},
+            onRemove: () {},
+          ),
+        ),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Проверяем, что название и цена отображаются
+    expect(find.text('Тестовое блюдо'), findsOneWidget);
+    expect(find.text('123.45₽'), findsOneWidget);
   });
 }
